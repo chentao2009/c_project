43bf52da6fc11fb60f4245fd53b70c3d3bdf7f0c
sihucun <18629504578@163.com>
2025-05-23 19:28:39 +0800

    带星星的hello world

--- hello.c
+++ hello.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+const int STAR = 15;
+
+void printStar(int count) {
+    if (count <= 0)
+    {
+        printf("星星数量不能小于等于0\n");
+        return;
+    }
+    for (int i = 0; i < count; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+
+void hello()
+{
+    printStar(STAR);
+
+    printf("Hello World!\n");
+
+    printStar(STAR);
+}
 
 int main()
 {
-    printf("Hello World!\n");
+    hello();
     return 0;
 }
